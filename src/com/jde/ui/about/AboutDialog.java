c9c64f84e54601e6ccb208c6ab0c656357bdb375
Julien Delacou <julien.delacou@gmail.com>
2013-01-13 09:49:45 +0100

    about: fix and add documentation

--- src/com/jde/ui/about/AboutDialog.java
+++ src/com/jde/ui/about/AboutDialog.java
@@ -2,19 +2,43 @@ package com.jde.ui.about;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+/**
+ * AboutDialog holding a AboutPane.
+ */
 public class AboutDialog extends JDialog {
+
 	private static final long serialVersionUID = -6325858152481736231L;
 
-	public AboutDialog(JFrame owner, String app, String version, String author,
+	/**
+	 * AboutDialog constructor
+	 *
+	 * @param owner
+	 *            - parent frame
+	 * @param app
+	 *            - name of the app
+	 * @param version
+	 *            - app version
+	 * @param author
+	 *            - developper name
+	 * @param email
+	 *            - contact email (can be null)
+	 * @param site
+	 *            - website url (can be null)
+	 * @param disclaimer
+	 *            - quick disclaimer/description (can be null)
+	 * @param icon
+	 *            - application icon (can be null)
+	 */
+	public AboutDialog(Frame owner, String app, String version, String author,
 			String email, String site, String disclaimer, ImageIcon icon) {
 		super(owner, app, true);
 
--- src/com/jde/ui/about/AboutPane.java
+++ src/com/jde/ui/about/AboutPane.java
@@ -7,10 +7,31 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/**
+ * AboutPane holding details about a software design.
+ */
 public class AboutPane extends JPanel {
 
 	private static final long serialVersionUID = -7058001019495308674L;
 
+	/**
+	 * AboutPane constructor
+	 *
+	 * @param app
+	 *            - name of the app
+	 * @param version
+	 *            - app version
+	 * @param author
+	 *            - developper name
+	 * @param email
+	 *            - contact email (can be null)
+	 * @param site
+	 *            - website url (can be null)
+	 * @param disclaimer
+	 *            - quick disclaimer/description (can be null)
+	 * @param icon
+	 *            - application icon (can be null)
+	 */
 	public AboutPane(String app, String version, String author, String email,
 			String site, String disclaimer, ImageIcon icon) {
 		super(new FlowLayout(FlowLayout.LEFT));
@@ -20,8 +41,7 @@ public class AboutPane extends JPanel {
 		a.add(new JLabel(app + (version != null ? " - version " + version : "")));
 		a.add(new JLabel("Author: " + author
 				+ (email != null ? " - email: <" + email + ">" : "")));
-		a.add(new JLabel(site != null ? "<html><body>Pease visit <a href=\""
-				+ site + "\"" + site + "</a></body></html>" : ""));
+		a.add(new JLabel(site != null ? "Pease visit " + site : ""));
 		a.add(new JLabel(disclaimer));
 
 		this.add(new JLabel(icon));
